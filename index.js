844a19a3c5faf51503c617b3ecb04c148e680e70
Minsik Choi <minsik.choi@anu.edu.au>
2024-07-25 17:50:36 +1000

    changed the port

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const express = require('express');
 const { Configuration, OpenAIApi } = require('openai');
 
 const app = express();
-const port = process.env.PORT || 5500;
+const port = process.env.PORT || 3000;
 
 // Set up OpenAI configuration
 const configuration = new Configuration({
