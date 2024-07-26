3e68d818d29bc7caa28e5ee8fd48e2d932098b86
Minsik Choi <minsik.choi@anu.edu.au>
2024-07-26 11:14:25 +1000

    url for fetch

--- public/script.js
+++ public/script.js
@@ -3,7 +3,7 @@ document.getElementById('text-form').addEventListener('submit', async (event) =>
     const prompt = document.getElementById('text-prompt').value;
 
     try {
-        const response = await fetch('/generate-text', {
+        const response = await fetch('http://localhost:5500/generate-text', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
@@ -27,7 +27,7 @@ document.getElementById('image-form').addEventListener('submit', async (event) =
     const prompt = document.getElementById('image-prompt').value;
 
     try {
-        const response = await fetch('/generate-image', {
+        const response = await fetch('http://localhost:5500/generate-image', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
